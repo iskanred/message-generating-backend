a2cd65019e720a933b1efb4d8165782e2fd24340
iskanred <nafikov.iskander@mail.ru>
2022-04-08 19:20:45 +0300

    Create repository

---
+++ src/main/java/dev/iskandev/messagegeneratingbackend/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package dev.iskandev.messagegeneratingbackend.repository;
+
+
+import dev.iskandev.messagegeneratingbackend.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+}
