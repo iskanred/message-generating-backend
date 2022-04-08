da1010853034a2c8cfc094abfa27c858119ecabb
iskanred <nafikov.iskander@mail.ru>
2022-04-08 19:36:40 +0300

    Add logger to controller

--- src/main/java/dev/iskandev/messagegeneratingbackend/controller/MessageController.java
+++ src/main/java/dev/iskandev/messagegeneratingbackend/controller/MessageController.java
@@ -3,6 +3,8 @@ package dev.iskandev.messagegeneratingbackend.controller;
 import dev.iskandev.messagegeneratingbackend.exception.MessageNotFoundException;
 import dev.iskandev.messagegeneratingbackend.model.Message;
 import dev.iskandev.messagegeneratingbackend.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message")
 public class MessageController {
 
+    private final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
     private final MessageService service;
 
     @Autowired
@@ -22,11 +26,13 @@ public class MessageController {
 
     @PostMapping
     public void createMessage() {
+        logger.info("Create message request has received");
         service.createMessage();
     }
 
     @GetMapping
     public Message getLastMessage() throws MessageNotFoundException {
+        logger.info("Get last message request has been received");
         return service.getLastMessage();
     }
 }
